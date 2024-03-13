d072d21940df1719ee52da7477cb6c851e1aece6
ikarthi2108 <karthicloud6@gmail.com>
2024-03-13 16:57:15 +0530

    Add project files.

---
+++ Task1/crud.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="crud.aspx.cs" Inherits="Task1.crud" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CRUD Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        form {
+            width: 60%;
+            margin: 50px auto;
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+        }
+
+        button {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+        table {
+            width: 100%;
+            margin-top: 20px;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #4caf50;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <label for="tableName">Name:</label>
+                <asp:TextBox ID="tableName" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label for="columnName">Column:</label>
+                <asp:TextBox ID="columnName" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label for="dataType">Data Type:</label>
+                <asp:TextBox ID="dataType" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Button ID="btnCreate" runat="server" Text="Create" OnClick="btnCreate_Click" />
+            </div>
+        </div>
+        <div>
+            <label for="username">Username:</label>
+            <asp:TextBox ID="username" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Create" OnClick="btnInsert_Click" />
+        </div>
+        <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="False" OnRowDeleting="GridView2_RowDeleting"
+            DataKeyNames="name" OnRowEditing="GridView2_RowEditing" OnRowCancelingEdit="GridView2_RowCancelingEdit"
+            OnRowUpdating="GridView2_RowUpdating" AllowPaging="True" PageSize="10">
+            <Columns>
+                <asp:BoundField DataField="name" HeaderText="Name" SortExpression="name" />
+                <asp:CommandField ShowDeleteButton="True" ButtonType="Button" DeleteText="Delete" />
+                <asp:CommandField ShowEditButton="True" ButtonType="Button" EditText="Edit" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Task1/crud.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace Task1
+{
+    public partial class crud : System.Web.UI.Page
+    {
+        string connectionString = "Data Source=DESKTOP-ME9JBB3\\SQL2019;Initial Catalog=task1;Integrated Security=True;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTable();
+            }
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            string query = $"Create Table {tableName.Text} ({columnName.Text} {dataType.Text});";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string InsertQuery = $"Insert Into UserTable  Values('{username.Text}');";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(InsertQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            LoadTable(); // Reload the GridView after insertion
+        }
+
+        protected void GridView2_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string usernameToDelete = GridView2.DataKeys[e.RowIndex].Values["name"].ToString();
+
+            string deleteQuery = $"DELETE FROM UserTable WHERE name = '{usernameToDelete}';";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(deleteQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            LoadTable(); // Reload the GridView after deletion
+        }
+
+        protected void GridView2_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView2.EditIndex = e.NewEditIndex;
+            LoadTable(); // Reload the GridView in edit mode
+        }
+
+        protected void GridView2_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView2.EditIndex = -1;
+            LoadTable(); // Reload the GridView after canceling edit
+        }
+
+        protected void GridView2_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string updatedUsername = ((TextBox)GridView2.Rows[e.RowIndex].Cells[0].Controls[0]).Text;
+            string originalUsername = GridView2.DataKeys[e.RowIndex].Values["name"].ToString();
+
+            string updateQuery = $"UPDATE UserTable SET name = '{updatedUsername}' WHERE name = '{originalUsername}';";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            GridView2.EditIndex = -1;
+            LoadTable(); // Reload the GridView after updating
+        }
+
+        protected void LoadTable()
+        {
+            string selectQuery = $"SELECT * FROM UserTable;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    GridView2.DataSource = dt;
+                    GridView2.DataBind();
+                }
+            }
+        }
+    }
+}
