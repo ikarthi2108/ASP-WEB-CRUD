fc6582715642d24003a6c3a16030b75f5b4441ec
ikarthi2108 <karthicloud6@gmail.com>
2024-03-27 17:11:13 +0530

    commit 2

--- Task1/About.aspx
+++ Task1/About.aspx
@@ -1,9 +1,89 @@
-﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Task1.About" %>
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Asp_Training.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <main aria-labelledby="title">
-        <h2 id="title"><%: Title %>.</h2>
-        <h3>Your application description page.</h3>
-        <p>Use this area to provide additional information.</p>
+        <div class="row mb-5">
+            <h3 class="mb-3">Data Operations using Stored Procedures</h3>
+            <div class="col-6 d-flex flex-column justify-content-start align-items-center gap-3">
+                <div class="form-group">
+                    <asp:Label ID="Label1" runat="server" Text="User ID"></asp:Label>
+                    <asp:TextBox ID="TextBox1" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="Label2" runat="server" Text="First Name"></asp:Label>
+                    <asp:TextBox ID="TextBox2" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="Label3" runat="server" Text="Last Name"></asp:Label>
+                    <asp:TextBox ID="TextBox3" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="Label4" runat="server" Text="User Name"></asp:Label>
+                    <asp:TextBox ID="TextBox4" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <div class="col-6 d-flex flex-column justify-content-center align-items-center gap-3">
+                <div class="form-group">
+                    <asp:Label ID="Label5" runat="server" Text="Email"></asp:Label>
+                    <asp:TextBox ID="TextBox5" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="Label6" runat="server" Text="Designation"></asp:Label>
+                    <asp:TextBox ID="TextBox6" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="Label7" runat="server" Text="Department"></asp:Label>
+                    <asp:DropDownList ID="DropDownList1" runat="server" CssClass="form-control">
+                        <asp:ListItem>Development</asp:ListItem>
+                        <asp:ListItem>Testing</asp:ListItem>
+                        <asp:ListItem>Marketing</asp:ListItem>
+                        <asp:ListItem>Management</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center align-items-center gap-5 mt-3">
+                <asp:Button ID="Button1" class="btn btn-primary" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+                <asp:Button ID="Button2" class="btn btn-success" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+                <asp:Button ID="Button3" class="btn btn-danger" runat="server" Text="Delete" OnClick="btnDelete_Click"/>
+            </div>
+        </div>
+        <div class="row">
+            <h3 class="mb-3">User Details</h3>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" CssClass="custom-grid">
+                <EmptyDataTemplate>
+                    <div>No data available</div>
+                </EmptyDataTemplate>
+                <Columns>
+                    <asp:BoundField HeaderText="User Id" DataField="UserId" />
+                    <asp:BoundField HeaderText="First Name" DataField="Firstname" />
+                    <asp:BoundField HeaderText="Last Name" DataField="Lastname" />
+                    <asp:BoundField HeaderText="User Name" DataField="Username" />
+                    <asp:BoundField HeaderText="Email" DataField="Email" />
+                    <asp:BoundField HeaderText="Designation" DataField="Designation" />
+                    <asp:BoundField HeaderText="Department" DataField="Department" />
+                    <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                            <asp:Button ID="Button4" class="btn btn-primary" runat="server" Text="Select" OnClick="btnSelect_Click" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
     </main>
+
+    <style>
+        .custom-grid {
+            border-collapse: separate;
+            border-spacing: 10px;
+            margin: 0 auto;
+        }
+
+            .custom-grid th,
+            .custom-grid td {
+                padding: 10px;
+                text-align: center;
+                border-left: 1px solid;
+                border-bottom: 1px solid;
+            }
+    </style>
 </asp:Content>
--- Task1/About.aspx.cs
+++ Task1/About.aspx.cs
@@ -1,17 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
-namespace Task1
+namespace Asp_Training
 {
     public partial class About : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetUsers();
+            }
+        }
 
+        SqlConnection connection = new SqlConnection("Data Source = DESKTOP-ME9JBB3\\SQL2019; Initial Catalog = task1; Integrated Security = True");
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            int uid = int.Parse(TextBox1.Text);
+            string fname = TextBox2.Text;
+            string lname = TextBox3.Text;
+            string uname = TextBox4.Text;
+            string email = TextBox5.Text;
+            string desig = TextBox6.Text;
+            string dept = DropDownList1.SelectedValue;
+            SqlCommand command = new SqlCommand("exec AddUser_sp '" + uid + "','" + fname + "','" + lname + "','" + uname + "','" +
+                 email + "','" + desig + "','" + dept + "'", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Inserted Successfully')", true);
+            GetUsers();
+        }
+
+        protected void GetUsers()
+        {
+            SqlCommand command = new SqlCommand("exec ViewUsers_sp", connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+            GridView1.DataSource = dataSet;
+            GridView1.DataBind();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int uid = int.Parse(TextBox1.Text);
+            string fname = TextBox2.Text;
+            string lname = TextBox3.Text;
+            string uname = TextBox4.Text;
+            string email = TextBox5.Text;
+            string desig = TextBox6.Text;
+            string dept = DropDownList1.SelectedValue;
+            SqlCommand command = new SqlCommand("exec UpdateUser_sp '" + uid + "','" + fname + "','" + lname + "','" + uname + "','" +
+                 email + "','" + desig + "','" + dept + "'", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Updated Successfully')", true);
+            GetUsers();
+        }
+
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            Button btnSelect = (Button)sender;
+            GridViewRow row = (GridViewRow)btnSelect.NamingContainer;
+            if (row != null)
+            {
+                int rowIndex = row.RowIndex;
+
+                if (rowIndex >= 0 && rowIndex < GridView1.Rows.Count)
+                {
+                    GridViewRow selectedRow = GridView1.Rows[rowIndex];
+                    int uid = int.Parse(selectedRow.Cells[0].Text);
+                    string fname = selectedRow.Cells[1].Text;
+                    string lname = selectedRow.Cells[2].Text;
+                    string uname = selectedRow.Cells[3].Text;
+                    string email = selectedRow.Cells[4].Text;
+                    string desig = selectedRow.Cells[5].Text;
+                    string dept = selectedRow.Cells[6].Text;
+                    TextBox1.Text = uid.ToString();
+                    TextBox2.Text = fname;
+                    TextBox3.Text = lname;
+                    TextBox4.Text = uname;
+                    TextBox5.Text = email;
+                    TextBox6.Text = desig;
+                    DropDownList1.SelectedValue = dept;
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Retrival Failed')", true);
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int uid = int.Parse(TextBox1.Text);
+            SqlCommand command = new SqlCommand("exec DeleteUser_sp '" + uid + "'", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Deleted Successfully')", true);
+            GetUsers();
         }
     }
 }
